a7df312b2b980ff69d5b2fa797046c8496274b4f
Serhii-progrmr <sergionnprog@gmail.com>
2023-06-02 14:51:38 +0300

    task 6_15 added

--- Homework/Homework 5/Homework 5.py
+++ Homework/Homework 5/Homework 5.py
@@ -56,24 +56,106 @@
 #     return found_articles
 
 ## task 3_15
-phone = "    +38(050)123-32-34"
+# phone = "    +38(050)123-32-34"
 #     "     0503451234",
 #     "(050)8889900",
 #     "38050-111-22-22",
 #     "38050 111 22 11   "
 
-def sanitize_phone_number(phone):
+# def sanitize_phone_number(phone):
     
-    new_ph_n = ''
-    phone.strip()
-    remove_char = ('+', '(', ')', '-', ' ')    
-    for i in phone:
-        if i not in remove_char:
-            new_ph_n = new_ph_n + i
+#     new_ph_n = ''
+#     phone.strip()
+#     remove_char = ('+', '(', ')', '-', ' ')    
+#     for i in phone:
+#         if i not in remove_char:
+#             new_ph_n = new_ph_n + i
 
-# #     return len(new_text)
-    # print(phone)
-    return new_ph_n
+# # #     return len(new_text)
+#     # print(phone)
+#     return new_ph_n
 
-print(sanitize_phone_number(phone))
-print(phone)
+# print(sanitize_phone_number(phone))
+# print(phone)
+
+# task 4_15
+#  функціz is_check_name приймає два параметри (fullname, first_name) і повертає
+# логічне значення True або False. Це результат перевірки,
+#  чи є рядок first_name префіксом рядка fullname.
+
+# def is_check_name(fullname, first_name):
+#    # if  first_name == "":
+#    #     return True
+#    # else:
+#    #     return False
+# #def is_check_name(fullname, first_name):
+#     return fullname.startswith(first_name)
+
+# task 5_15
+# def sanitize_phone_number(phone):
+#     new_phone = (
+#         phone.strip()
+#         .removeprefix("+")
+#         .replace("(", "")
+#         .replace(")", "")
+#         .replace("-", "")
+#         .replace(" ", "")
+#     )
+#     return new_phone
+
+# def get_phone_numbers_for_countries(list_phones):
+#     sanitazed_phones = [sanitize_phone_number(phone) for phone in list_phones]
+#     phone_dict = {"UA": [], "JP": [], "TW": [], "SG": []}
+#     for phone in sanitazed_phones:
+#         if phone[:2] == '81':
+#             phone_dict["JP"].append(phone)
+#         elif phone[:2] == '65':
+#             phone_dict["SG"].append(phone)
+#         elif phone[:3] == '886':
+#             phone_dict["TW"].append(phone)
+#         else:
+#             phone_dict["UA"].append(phone)
+#     return phone_dict
+
+# 2-nd variant (is not working)
+# def sanitize_phone_number(phone):
+#     new_phone = (
+#         phone.strip()
+#         .removeprefix("+")
+#         .replace("(", "")
+#         .replace(")", "")
+#         .replace("-", "")
+#         .replace(" ", "")
+#     )
+#     return new_phone
+
+ 
+# def get_phone_numbers_for_countries(list_phones):
+#     phone_codes = {
+#         'JP': '+81',
+#         'SG': '+65',
+#         'TW': '+886',
+#         'UA': '+380'
+#     }
+
+#     phone_country = {
+#         'UA': [],
+#         'JP': [],
+#         'TW': [],
+#         'SG': []
+#     }
+
+#     for phone in list_phones:
+#         phone = sanitize_phone_number(phone)
+#         prefix = phone[:2] if len(phone) >= 2 else phone[:3]
+#         country = phone_codes.get(prefix, 'UA')
+#         if phone not in phone_country[country]:
+#             phone_country[country].append(phone)
+
+#     return phone_country
+
+# 6_15
+def is_spam_words(text, spam_words, space_around=False):
+    snt_text = text.lower()
+    
+
